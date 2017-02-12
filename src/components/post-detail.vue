56e2463c0e105fcdf1098ccc03d4885b2a8b1d23
Philipp Gfeller <tuelsch@gmail.com>
2017-02-12 11:34:14 +0100

    A posts detail

--- src/components/post-detail.vue
+++ src/components/post-detail.vue
@@ -1,17 +1,36 @@
 <template lang="pug">
-
+  div.post-detail
+    h1 {{post.title}}
+    p
+      img.post-detail--image(v-for="image in post.images" v-bind:src="image")
+    auth-guard
+      div(slot="auth")
+        create-comment(v-bind:post="post")
+    comments
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
+  import AuthGuard from 'components/auth-guard';
+  import CreateComment from 'components/create-comment';
+  import Comments from 'components/comments';
+  import { database } from 'config/firebase';
+  import router from 'config/router';
 
-			};
-		},
+	export default {
+    created() {
+      this.$bindAsObject('post', database.ref(`/posts/${this.$route.params.id}`));
+    },
+
+    components: {
+      CreateComment,
+      AuthGuard,
+      Comments,
+    },
 	};
 </script>
 
 <style lang="scss">
-
+  .post-detail--image {
+    max-width: 600px;
+  }
 </style>
