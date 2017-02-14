f8e5f814c1ba09d96813cf54b7411536d7cd0b67
Philipp Gfeller <tuelsch@gmail.com>
2017-02-14 18:53:34 +0100

    Font

--- src/App.vue
+++ src/App.vue
@@ -5,87 +5,92 @@
       router-link(to="/posts") Posts
       router-link(to="/profile") Profile
       auth-guard
-        div(slot="auth") Logged in as "{{user.displayName}}"
+        div(slot="auth") Logged in as "{{user.displayName || user.email}}"
           button(@click="signOut") logout
         div(slot="no-auth")
           router-link(to="/login") login
     main
+      styleguide
       router-view
     footer
 </template>
 
 <script>
-import store from 'config/store';
-import AuthGuard from 'components/auth-guard';
-import { auth } from 'config/firebase';
+    import store from 'config/store';
+    import AuthGuard from 'components/auth-guard';
+    import Styleguide from 'components/styleguide';
+    import {
+        auth
+    } from 'config/firebase';
 
-export default {
-  name: 'app',
-  created() {
+    export default {
+        name: 'app',
+        created() {
 
-    // Listen to auth changes at firebase
-    auth
-      .onAuthStateChanged((user) => {
-        this.isLoading = false;
+            // Listen to auth changes at firebase
+            auth
+                .onAuthStateChanged((user) => {
+                    this.isLoading = false;
 
-        // Dispatch according to auth state
-        if (user) {
-          store.commit('LOGIN', {user});
-        } else {
-          store.commit('LOGOUT');
+                    // Dispatch according to auth state
+                    if (user) {
+                        store.commit('LOGIN', {
+                            user
+                        });
+                    } else {
+                        store.commit('LOGOUT');
+                    }
+                });
+        },
+        computed: {
+            user() {
+                return this.$store.state.auth.user
+            },
+        },
+        methods: {
+            signOut() {
+                auth.signOut();
+            }
+        },
+        components: {
+            AuthGuard,
+            Styleguide,
         }
-      });
-  },
-  computed: {
-    user() {return this.$store.state.auth.user},
-  },
-  methods: {
-    signOut() {
-      auth.signOut();
     }
-  },
-  components: {
-    AuthGuard,
-  }
-}
 </script>
 
 <style lang="scss">
-  html {
-    height: 100%;
-    font-size: 10px;
-  }
-
-  body {
-    margin: 0;
-    min-height: 100%;
-    display: flex;
-    flex-direction: column;
-    background-color: #353535;
-    font-family: 'Open Sans', sans-serif;
-    color: white;
-    font-size: 20px;
-  }
-
-  main {
-    flex-grow: 1;
-  }
-
-  a {
-    display: inline-block;
-    color: lightblue;
-    text-decoration: none;
-
-    &.active {
-      text-decoration: underline;
+    @import 'src/sass/text';
+    html {
+        height: 100%;
     }
-
-    & + a {
-      margin-left: 1em;
+    
+    body {
+        margin: 0;
+        min-height: 100%;
+        display: flex;
+        flex-direction: column;
+        background-color: #353535;
+        color: white;
     }
-  }
-
-  img {
-    max-width: 100%;
-  }
+    
+    main {
+        flex-grow: 1;
+    }
+    
+    a {
+        display: inline-block;
+        color: lightblue;
+        text-decoration: none;
+        &.active {
+            text-decoration: underline;
+        }
+        &+a {
+            margin-left: 1em;
+        }
+    }
+    
+    img {
+        max-width: 100%;
+    }
 </style>
---
+++ src/components/styleguide.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+- var panagram = 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+div.styleguide
+    h1= 'h1 - '+ panagram
+    h2= 'h2 - '+ panagram
+    h3= 'h3 - '+ panagram
+    h4= 'h4 - '+ panagram
+    h5= 'h5 - '+ panagram
+    h6= 'h6 - '+ panagram
+
+    p= 'paragraph - '+ panagram
+
+    p.darkgreen-text= 'colored paragraph - '+ panagram
+
+    p
+        strong= 'strong - '+ panagram
+    p
+        i= 'italic - '+ panagram
+    p
+        small= 'small - '+ panagram
+    p.uppercase
+        | Uppercase text
+    p
+        abbr(title='Abbreviation') Abbr.
+    p 20m
+        sup 2
+    p H
+        sub 2
+    blockquote Use the force, Harry.
+        small Gandalf
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss">
+
+</style>
